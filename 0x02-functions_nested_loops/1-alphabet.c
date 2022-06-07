be16995c93fc39d7a6b41728f66b0e6c0937365f
Bernard Addy-Sackey <baddysackey5@gmail.com>
2022-06-07 15:23:02 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * main- prints the alphabet
+ * print_alphabet- prints the alphabet
  * in lowercase
  * followed by a new line
- * Return: Always 0 (Success)
  */
-
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,11 @@
+#ifndef main_h_included
+#define main_h_included
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int c);
+
+#endif
