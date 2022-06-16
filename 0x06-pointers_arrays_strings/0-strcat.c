3a2f0a0e4ebce1ff87544bddd847120d6dd58c39
boypaida12 <baddysackey5@gmail.com>
2022-06-16 18:48:55 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,10 @@ char *_strcat(char *dest, char *src)
 	int i, j;
 
 	for (i = 0; dest[i] != '\0'; i++)
-		for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
-		{
-		}
+	{
+	}
+	for (j = 0; (dest[i + j] = *src++) != '\0'; j++)
+	{
+	}
 	return (dest);
 }
