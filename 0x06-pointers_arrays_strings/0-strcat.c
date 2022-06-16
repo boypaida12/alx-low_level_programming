aad7a5668f4a2019f344e797903777154accec8c
boypaida12 <baddysackey5@gmail.com>
2022-06-16 18:26:23 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		for (j = 0; src[j] != '\0'; j++, i++)
+		{
+			dest[i] = src[j];
+		}
+	dest[i] = '\0';
+	return (dest);
+}
